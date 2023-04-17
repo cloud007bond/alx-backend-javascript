d0cf1e4ca2b82faaab33c18c3d9e71950609697a
Ezinwa charles <charleskelvin12@gmail.com>
2023-04-17 08:27:44 -0700

    task

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export function taskFirst() {
+        const task = 'Iprefer const when I CAN.';
+        return task;
+}
+
+export function getLast() {
+        return 'is okay';
+}
+
+export function teskNext() {
+        let combination = 'But sometimes let';'
+        combination += getLast();
+
+        return combination;
+}
